4453a68784f6d712fcb9914c89fee75d50156dae
谢子豪 <jarodxie@foxmail.com>
2021-08-21 18:06:30 +0800

    缓存

--- demo07-缓存/src/index.js
+++ demo07-缓存/src/index.js
@@ -1,18 +1,14 @@
+import _ from 'lodash';
+//import Print from './print';
 
-function getComponent() {
+function component() {
   const element = document.createElement('div');
-  
-  return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
 
-      element.innerHTML = _.join(['Hello1', 'webpack2'], ' ');
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //element.onclick = Print.bind(null, 'Hello webpack!');
 
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
+  return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
